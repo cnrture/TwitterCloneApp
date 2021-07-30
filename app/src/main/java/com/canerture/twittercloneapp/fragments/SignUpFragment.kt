c8ebd4dce02dc7106558836615149dc2721a9783
Caner Türe <cnrture@gmail.com>
2021-07-31 00:32:00 +0300

    Login Activity and Fragments Created

---
+++ app/src/main/java/com/canerture/twittercloneapp/fragments/SignUpFragment.kt
@@ -0,0 +1,25 @@
+package com.canerture.twittercloneapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.canerture.twittercloneapp.R
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+}
